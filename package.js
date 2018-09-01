89c30c9c3771ce96527dc0125cb5e91eb1507bb8
Abigail Watson <abigailwatson@Abigails-Mac-mini-379101.local>
2018-09-01 12:14:41 -0500

    simpl-schema updates; hipaa-logger support

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-endpoint',
-  version: '3.0.16',
+  version: '3.1.0',
   summary: 'HL7 FHIR Resource - Endpoint',
   git: 'https://github.com/clinical-meteor/hl7-resource-endpoint',
   documentation: 'README.md'
@@ -11,7 +11,11 @@ Package.onUse(function (api) {
 
   api.use('meteor-platform');
   api.use('mongo');
-  api.use('aldeed:simple-schema@1.3.3');
+
+  api.use('aldeed:collection2@3.0.0');
+  api.use('clinical:hl7-resource-datatypes@4.0.0');
+  api.use('clinical:hl7-resource-bundle@1.4.0');
+
   api.use('simple:json-routes@2.1.0');
   api.use('momentjs:moment@2.17.1');
   api.use('ecmascript@0.9.0');
@@ -20,9 +24,7 @@ Package.onUse(function (api) {
   api.use('react-meteor-data@0.2.15');
 
   api.use('clinical:glass-ui@2.2.4');
-  api.use('clinical:base-model@1.3.5');
-  api.use('clinical:hl7-resource-datatypes@3.0.1');
-  api.use('clinical:hl7-resource-bundle@1.3.14');
+  api.use('clinical:base-model@1.4.0');
   api.use('clinical:alert@2.1.3');
 
   api.addFiles('lib/Endpoints.js', ['client', 'server']);
@@ -39,3 +41,8 @@ Package.onUse(function (api) {
 
   api.mainModule('index.jsx', 'client');
 });
+
+
+Npm.depends({
+  "simpl-schema": "1.5.3"
+})
