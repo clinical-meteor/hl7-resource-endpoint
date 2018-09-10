900ab7a56d8d50e53d737519b9f12aa362ca573f
Abigail Watson <abigailwatson@Abigails-Mac-mini-380428.local>
2018-09-09 20:57:13 -0500

    sync

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-endpoint',
-  version: '3.1.1',
+  version: '3.1.2',
   summary: 'HL7 FHIR Resource - Endpoint',
   git: 'https://github.com/clinical-meteor/hl7-resource-endpoint',
   documentation: 'README.md'
@@ -44,5 +44,7 @@ Package.onUse(function (api) {
 
 
 Npm.depends({
-  "simpl-schema": "1.5.3"
+  "simpl-schema": "1.5.3",
+  "moment": "2.22.2",
+  "lodash": "4.17.4"
 })
